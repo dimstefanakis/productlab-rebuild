b66b80ab3e7ce87805de84b83ba80f11a141924a
Dimitris Stefanakis <jimstef@outlook.com>
2022-02-22 20:22:22 +0200

    fixed preview page

--- slices/Blog/index.tsx
+++ slices/Blog/index.tsx
@@ -5,18 +5,20 @@ import { Image } from "@chakra-ui/image";
 import { RichText, Link } from "prismic-reactjs";
 import { BlogProps } from "./interface";
 
-const Blog = ({ slice }: any) => (
-  <Flex flexFlow="column">
-    {slice?.items.map((item: any, i: number) => {
-      return (
-        <Flex key={i} flexFlow="column">
-          <Text>{item.title}</Text>
-          <ChakraLink href={Link.url(item.link)}>My Link</ChakraLink>
-          <Image src={item.image.url} alt={item.image.alt} />
-        </Flex>
-      );
-    })}
-    {/* <span className="title">
+const Blog = ({ slice }: any) => {
+  console.log("slice", slice)
+  return (
+    <Flex flexFlow="column">
+      <Text>{slice.primary.title}</Text>
+      <ChakraLink href={Link.url(slice.primary.link.url)}>My Link</ChakraLink>
+      <Image
+        src={slice.primary.previewimage.url}
+        alt={slice.primary.previewimage.alt}
+      />
+      {slice?.items.map((item: any, i: number) => {
+        return <Flex key={i} flexFlow="column"></Flex>;
+      })}
+      {/* <span className="title">
       {slice.primary.title ? (
         <RichText render={slice.primary.title} />
       ) : (
@@ -38,7 +40,8 @@ const Blog = ({ slice }: any) => (
         color: #8592e0;
       }
     `}</style> */}
-  </Flex>
-);
+    </Flex>
+  );
+};
 
 export default Blog;
