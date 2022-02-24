b0677e7d7f3eccc35ec4581791c9cfc022d960da
Dimitris Stefanakis <jimstef@outlook.com>
2022-02-24 23:27:13 +0200

    updated prismic slices

---
+++ .slicemachine/assets/slices/BlogImage/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/BlogImage';
+
+export default {
+  title: 'slices/BlogImage'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"blog_image","items":[],"primary":{"imageTitle":"brand open-source infomediaries","image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1593642633279-1796119d5482?w=900&h=500&fit=crop"},"imageDescription":"iterate compelling platforms"},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
---
+++ .slicemachine/assets/slices/BlogText/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/BlogText';
+
+export default {
+  title: 'slices/BlogText'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"blog_text","items":[],"primary":{"body":[{"type":"paragraph","text":"Dolore do eiusmod adipisicing reprehenderit elit sit cillum sunt voluptate culpa. Excepteur velit mollit sit deserunt aute et.","spans":[]}]},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
