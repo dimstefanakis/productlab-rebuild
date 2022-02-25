712f2d664eec3bfbc14edabe1ad4046727b8f6c5
Dimitris Stefanakis <jimstef@outlook.com>
2022-02-25 23:27:17 +0200

    ts fix

--- src/flat/BlogPost/index.tsx
+++ src/flat/BlogPost/index.tsx
@@ -1,12 +1,12 @@
 import { Flex, Box, Text, Heading } from "@chakra-ui/layout";
 import { RichText } from "prismic-reactjs";
 
-function BlogPost({post}){
-    return(
-        <Flex>
-            <RichText />
-        </Flex>
-    )
+function BlogPost({ post }: any) {
+  return (
+    <Flex>
+      <RichText />
+    </Flex>
+  );
 }
 
 export default BlogPost;
