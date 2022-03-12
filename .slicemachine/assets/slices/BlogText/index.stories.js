2409538d9c8a89b4f5a9eb070bb42123af0e9ae7
Dimitris Stefanakis <jimstef@outlook.com>
2022-03-12 21:20:47 +0200

    updated slices

--- .slicemachine/assets/slices/BlogText/index.stories.js
+++ .slicemachine/assets/slices/BlogText/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"blog_text","items":[],"primary":{"body":[{"type":"paragraph","text":"Esse ex in Lorem esse commodo anim proident eiusmod ipsum nostrud fugiat amet culpa anim duis.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"blog_text","items":[],"primary":{"body":[{"type":"paragraph","text":"Nulla consectetur et qui irure. Ex duis Lorem ea do.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
