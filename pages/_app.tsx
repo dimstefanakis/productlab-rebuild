4ecbfd490ddac37b796960eb44480619a9ef2ead
Dimitris Stefanakis <jimstef@outlook.com>
2022-03-12 23:38:53 +0200

    fixed focus issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,24 @@ import { useState, useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { useColorMode } from "@chakra-ui/color-mode";
+import { Global, css } from "@emotion/react";
 import theme from "../src/theme";
+import "focus-visible/dist/focus-visible";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../src/flat/Layout";
 import { store } from "../src/store";
+
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 interface AppWrapperProps {
   children: JSX.Element;
@@ -15,6 +28,7 @@ interface AppWrapperProps {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Global styles={GlobalStyles} />
       <ChakraProvider theme={theme}>
         <Layout>
           <AppWrapper>
